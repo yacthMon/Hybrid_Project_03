16d8ef0648f3892c980631127ece49679b05baa0
yacth_Mon <yacthmon@gmail.com>
2017-11-28 02:27:06 +0700

    Add first page

---
+++ src/pages/first/first.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FirstPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-first',
+  templateUrl: 'first.html',
+})
+export class FirstPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FirstPage');
+  }
+
+}
